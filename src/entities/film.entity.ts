acc577abe5530c87da65bba2f03e84548df4e800
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-10 18:32:08 +0300

    add enums

--- src/entities/film.entity.ts
+++ src/entities/film.entity.ts
@@ -1,10 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
-import { ObjectType, Field, Int } from 'type-graphql'
+import { ObjectType, Field, Int, InputType } from 'type-graphql'
+import { registerEnumType } from "type-graphql";
 
 
 
+enum FilmRatings{
+PG13 = "PG-13",
+    NC17 = "NC-17",
+    R = "R",
+    G = "G",
+    PG = "PG"
+}
+
+
+registerEnumType(FilmRatings, {
+    name: "rating", // Mandatory
+    description: "The film rating", // Optional
+});
 
 @ObjectType()
+@InputType()
 @Entity()
 export class Film {
     @Field(() => Int)
@@ -39,9 +54,9 @@ export class Film {
     @Column({ type: "numeric" })
     replacement_cost: number;
 
-    @Field(()=>String)
-    @Column({ type: "text" })
-    rating: string;
+    @Field(()=>FilmRatings)
+    @Column({ type: "enum",enumName: "rating" })
+    rating: FilmRatings
 
     @Field(() => String)
     @Column({ type: "timestamp without time zone" })
@@ -55,7 +70,75 @@ export class Film {
     @Column({ type: "text" })
     fulltext: string;
 
+}
 
-  
+
+
+@InputType()
+export class FilmInput {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    film_id: number;
+
+    @Field(() => String)
+    @Column({ type: "text" })
+    title: string;
+
+    @Field(() => Int)
+    @Column({ type: "integer" })
+    release_year: number;
+
+    @Field(() => Int)
+    @Column({ type: "smallint" })
+    language_id: number;
+
+    @Field(() => Int)
+    @Column({ type: "smallint" })
+    rental_duration: number;
+
+    @Field(() => Int)
+    @Column({ type: "numeric" })
+    rental_rate: number;
+
+    @Field(() => Int)
+    @Column({ type: "smallint" })
+    length: number;
+
+    @Field(() => Int)
+    @Column({ type: "numeric" })
+    replacement_cost: number;
+    
+    
+    @Field(() => FilmRatings)
+    @Column({ type: "enum", enumName: "rating" })
+    rating: FilmRatings
+
+    @Field(() => String)
+    @Column({ type: "timestamp without time zone" })
+    last_update: Date;
+
+    @Field(() => [String])
+    @Column({ type: "text", array: true })
+    special_features: string[];
+
+    @Field(() => String)
+    @Column({ type: "text" })
+    fulltext: string;
 
 }
+
+
+// const new_film:FilmInput ={
+// "film_id":1001,
+// "title":'test',
+// "release_year":2000,
+// "language_id":1,
+// "rental_duration":1,
+// "rental_rate":1,
+// "length":1,
+// "replacement_cost":1,
+// "rating":'test',
+// "last_update":"12/12/2022",
+// "special_features":['test'],
+// "fulltext":'test'
+// }
--- src/resolvers/film/film_resolver.ts
+++ src/resolvers/film/film_resolver.ts
@@ -1,71 +1,107 @@
-import { Film } from "@/entities/film.entity";
+import { Film, FilmInput } from "@/entities/film.entity";
 import { AppDataSource } from "@/typeorm";
-import { Arg, Field, Int, ObjectType, Query, Resolver } from "type-graphql";
-import { MoreThan } from "typeorm";
+import { Arg, Field, InputType, Int, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import { Column, InsertResult, MoreThan, PrimaryGeneratedColumn, UpdateResult } from "typeorm";
 
-const filmRepository = AppDataSource.getRepository(Film)
+const filmRepository = AppDataSource.getRepository(Film);
 
 @ObjectType()
 export class PaginatedFilms {
-    @Field(() => [Film])
-    films: Film[]
-    @Field(()=>Boolean)
-    hasMore: boolean
-    @Field(()=>Int)
-    cursor: number
-    @Field(()=>Int)
-    total: number
+  @Field(() => [Film])
+  films: Film[];
+  @Field(() => Boolean)
+  hasMore: boolean;
+  @Field(() => Int)
+  cursor: number;
+  @Field(() => Int)
+  total: number;
 }
 
+// @InputType()
+// class FilmInput {
+//     @Field(() => Int)
+//     @PrimaryGeneratedColumn()
+//     film_id: number;
+
+//     @Field(() => String)
+//     @Column({ type: "text" })
+//     title: string;
+
+//     @Field(() => Int)
+//     @Column({ type: "integer" })
+//     release_year: number;
+// }
 
 @Resolver()
 export class FilmResolver {
-    @Query(() => [Film])
-    async allFilms(): Promise<Film[]> {
-        const films = await filmRepository.find()
-        return films
-    }
+  //  get all items
+  @Query(() => [Film])
+  async allFilms(): Promise<Film[]> {
+    const films = await filmRepository.find();
+    return films;
+  }
 
-    @Query(() => PaginatedFilms)
-    async filmList(
-        @Arg("cursor", () => Int) cursor:number,
-        @Arg("limit", () => Int, {nullable: true}) limit:number
-        ): Promise<PaginatedFilms> {
-        const realLimit = Math.min(100, limit)
-        const realLimitPlusOne = realLimit + 1
+  // get a paginated list
+  @Query(() => PaginatedFilms)
+  async filmList(
+    @Arg("cursor", () => Int) cursor: number,
+    @Arg("limit", () => Int, { nullable: true }) limit: number
+  ): Promise<PaginatedFilms> {
+    const realLimit = Math.min(100, limit);
+    const realLimitPlusOne = realLimit + 1;
+    const totalFilms = await filmRepository.count();
+    const films = await filmRepository.find({
+      take: realLimitPlusOne,
+      order: { film_id: "ASC" },
+      where: {
+        film_id: MoreThan(cursor),
+      },
+    });
+    const filmsWithCursor = films.slice(0, -1);
+    return {
+      films: filmsWithCursor,
+      hasMore: films.length === realLimitPlusOne,
+      total: totalFilms,
+      cursor: filmsWithCursor[filmsWithCursor.length - 1].film_id,
+    };
+  }
 
-        // console.log("limit",limit)
-        // console.log("realLimit",realLimit)
-        // console.log("realLimit",realLimit)
-        // console.log("realLimitPlusOne",realLimitPlusOne)
+  // get film by id
+  @Query(() => Film)
+  async film(@Arg("id", () => Int) id: number): Promise<Film | null> {
+    const film = await filmRepository.findOneBy({ film_id: id });
+    return film;
+  }
 
-        const totalFilms = await filmRepository.count()
-        const films = await filmRepository.find({
-            take: realLimitPlusOne,
-            order:{film_id: "ASC"},
-            where: {
-                film_id: MoreThan(cursor)
-            }
-        })
-        const filmsWithCursor = films.slice(0, -1)
-        return {
-            films: filmsWithCursor,
-            hasMore: films.length === realLimitPlusOne,
-            total: totalFilms,
-            cursor: (filmsWithCursor[filmsWithCursor.length-1].film_id)
-        }
-
-    }
-
-
-    @Query(() => Film)
-    async film(
-    @Arg("id", () => Int)id:number
-    ): Promise<Film|null> {
-        const film = await filmRepository.findOneBy({film_id: id})
-        return film
-    }
-
+// add film
+@Mutation(() => Film)
+async addFilm(@Arg("film",()=>FilmInput) film: Film): Promise<Film> {
+  return await filmRepository.save(film);
 }
 
+// // update film
+// @Mutation(() => Film)
+// async upsertFilm(@Arg('film', () => Film) film: Film): Promise<InsertResult> {
+//   const upserted =await filmRepository.upsert(film, ["film_id"]);
+//   return upserted
+// }
 
+// // update film
+// @Mutation(() => Film)
+// async updateFilm(@Arg('film', () => Film) film: Film): Promise<UpdateResult> {
+//   const updated =await filmRepository.update(film.film_id, film);
+//   return updated
+// }
+
+// delete film
+
+@Mutation(() => Boolean)
+async deleteFilm(@Arg('id', () => Int) id: number): Promise<boolean> {
+  try {
+    await filmRepository.delete(id);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+}
